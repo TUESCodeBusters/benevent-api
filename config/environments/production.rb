779d79f4e025e1940b7222d2332237ed2138cd60
Slavkata <slavkirilov00@gmail.com>
2017-05-12 15:13:05 +0300

    remove

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { host: 'tues.herokuapp.com' }
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
